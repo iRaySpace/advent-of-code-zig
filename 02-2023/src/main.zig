f083d7b9f3de71cd76a7a3d4a2db22b4edfd6e6d
irayspace <irayspacii@gmail.com>
2023-12-04 12:08:58 +0800

    fmt: use snake_cases on variables

--- 02-2023/src/main.zig
+++ 02-2023/src/main.zig
@@ -37,8 +37,8 @@ pub fn main() !void {
                 game_no = try getGameNo(x);
                 continue;
             }
-            const rgbCubes = try getRgbCubes(x);
-            if (rgbCubes[red_index] <= red_max and rgbCubes[green_index] <= green_max and rgbCubes[blue_index] <= blue_max) {
+            const rgb_cubes = try getRgbCubes(x);
+            if (rgb_cubes[red_index] <= red_max and rgb_cubes[green_index] <= green_max and rgb_cubes[blue_index] <= blue_max) {
                 sigma_game_no += game_no;
             }
         }
@@ -50,24 +50,24 @@ pub fn main() !void {
 }
 
 fn getGameNo(items: []const u8) !u8 {
-    const gameIndex = std.mem.indexOfDiff(u8, items, "Game ");
-    var gameNo = items[gameIndex.?..items.len];
-    return try std.fmt.parseInt(u8, gameNo, 10);
+    const index = std.mem.indexOfDiff(u8, items, "Game ");
+    var game_no = items[index.?..items.len];
+    return try std.fmt.parseInt(u8, game_no, 10);
 }
 
 fn getRgbCubes(items: []const u8) ![3]u8 {
     var rgb = [_]u8{ 0, 0, 0 };
     var digits = [_]u8{ 0, 0, 0 };
-    var digitIndex: u8 = 0;
+    var digit_index: u8 = 0;
     for (items) |item| {
         if (item == ' ') {
             continue;
         }
         if (std.ascii.isDigit(item)) {
-            digits[digitIndex] = item;
-            digitIndex += 1;
-        } else if (!std.ascii.isDigit(item) and digitIndex > 0) {
-            const number = try std.fmt.parseInt(u8, digits[0..digitIndex], 10);
+            digits[digit_index] = item;
+            digit_index += 1;
+        } else if (!std.ascii.isDigit(item) and digit_index > 0) {
+            const number = try std.fmt.parseInt(u8, digits[0..digit_index], 10);
             if (item == 'r' and number > rgb[0]) {
                 rgb[0] = number;
             } else if (item == 'g' and number > rgb[1]) {
@@ -75,7 +75,7 @@ fn getRgbCubes(items: []const u8) ![3]u8 {
             } else if (item == 'b' and number > rgb[2]) {
                 rgb[2] = number;
             }
-            digitIndex = 0;
+            digit_index = 0;
         }
     }
     return rgb;
